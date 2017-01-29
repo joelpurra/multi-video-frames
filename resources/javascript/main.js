0543a8f4b24bc15683d4f4b07c675e4685eb5b9a
Joel Purra <code+github@joelpurra.com>
2017-01-29 09:33:26 +0100

    Clean up the javascript a bit

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -6,12 +6,16 @@
             });
         },
     });
+
     $(function() {
         function addUrlInput() {
             var $urlInput = $("<input />", {
                 "type": "url",
                 "placeholder": "Copy and paste URL here",
-            }).data("random", getRandom()).appendTo($controls);
+            })
+                .data("random", getRandom())
+                .appendTo($controls);
+
             return $urlInput;
         }
 
@@ -20,24 +24,37 @@
         }
 
         function addVideoPanel($urlInput) {
-            var $videoPanel = $("<iframe />").addClass("videoPanel").data("$urlInput", $urlInput).appendTo($videos);
+            var $videoPanel = $("<iframe />").addClass("videoPanel")
+            .data("$urlInput", $urlInput)
+            .appendTo($videos);
+
             $urlInput.data("$videoPanel", $videoPanel).focus();
             setVideoSizes();
+
             return $videoPanel;
         }
 
         function updateVideoPanels() {
             var $videoPanels = $videos.find(".videoPanel"),
                 videoPanelCount = $videoPanels.length,
-            // TODO: make horizontal/vertical video panel count a setting
+
+                // TODO: make horizontal/vertical video panel count a setting.
                 videoPanelsHorizontalMax = 3,
                 videoPanelsVerticalMax = 3,
                 videoPanelsHorizontal = Math.min(Math.ceil(Math.sqrt(videoPanelCount)), videoPanelsHorizontalMax),
                 videoPanelsVertical = videoPanelsHorizontal === 0 ? 0 : Math.min(Math.ceil(videoPanelCount / videoPanelsHorizontal), videoPanelsVerticalMax);
-        // Clear all classes
-            $controls.attr("class", "").addClass("urlInputs-" + videoPanelsHorizontal).scrollToTop();
-        // Clear all classes
-            $videos.attr("class", "").addClass("videoPanelsHorizontal-" + videoPanelsHorizontal).addClass("videoPanelsVertical-" + videoPanelsVertical);
+
+            // Clear all classes.
+            $controls
+                .attr("class", "")
+                .addClass("urlInputs-" + videoPanelsHorizontal)
+                .scrollToTop();
+
+            // Clear all classes.
+            $videos
+                .attr("class", "")
+                .addClass("videoPanelsHorizontal-" + videoPanelsHorizontal)
+                .addClass("videoPanelsVertical-" + videoPanelsVertical);
         }
 
         function setVideoSizes() {
@@ -48,23 +65,26 @@
             $addUrl = $("#addUrl"),
             $videos = $("#videos"),
             loaded = [];
+
         $controls.scrollToTop();
+
         updateVideoPanels();
+
         $addUrl.click(function(evt) {
             var $target = $(evt.target),
                 $urlInput = addUrlInput(),
                 $videoPanel = addVideoPanel($urlInput);
         });
+
         $window.on("change", function(evt) {
-            function loadUrl() {
-                $videoPanel.attr("src", url);
-            }
             var $target = $(evt.target),
                 random = $target.data("random"),
                 url = $target.val(),
                 $videoPanel = $target.data("$videoPanel");
+
             $videos.scrollToTop();
-            loadUrl();
+
+            $videoPanel.attr("src", url);
         });
     });
 }(window, window.jQuery));
