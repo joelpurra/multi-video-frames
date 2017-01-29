c0cb9b8e6ee648503df806f7ee2f515cedab0c83
Joel Purra <code+github@joelpurra.com>
2017-01-29 08:56:42 +0100

    Move javascript to separate file

---
+++ resources/javascript/main.js
@@ -0,0 +1,70 @@
+(function(window, $) {
+    $.fn.extend({
+        scrollToTop: function() {
+            $("html, body").animate({
+                scrollTop: Math.max(0, $(this).offset().top),
+            });
+        },
+    });
+    $(function() {
+        function addUrlInput() {
+            var $urlInput = $("<input />", {
+                "type": "url",
+                "placeholder": "Copy and paste URL here",
+            }).data("random", getRandom()).appendTo($controls);
+            return $urlInput;
+        }
+
+        function getRandom() {
+            return Math.floor(Math.random() * 100000) + 100000;
+        }
+
+        function addVideoPanel($urlInput) {
+            var $videoPanel = $("<iframe />").addClass("videoPanel").data("$urlInput", $urlInput).appendTo($videos);
+            $urlInput.data("$videoPanel", $videoPanel).focus();
+            setVideoSizes();
+            return $videoPanel;
+        }
+
+        function updateVideoPanels() {
+            var $videoPanels = $videos.find(".videoPanel"),
+                videoPanelCount = $videoPanels.length,
+            // TODO: make horizontal/vertical video panel count a setting
+                videoPanelsHorizontalMax = 3,
+                videoPanelsVerticalMax = 3,
+                videoPanelsHorizontal = Math.min(Math.ceil(Math.sqrt(videoPanelCount)), videoPanelsHorizontalMax),
+                videoPanelsVertical = videoPanelsHorizontal === 0 ? 0 : Math.min(Math.ceil(videoPanelCount / videoPanelsHorizontal), videoPanelsVerticalMax);
+        // Clear all classes
+            $controls.attr("class", "").addClass("urlInputs-" + videoPanelsHorizontal).scrollToTop();
+        // Clear all classes
+            $videos.attr("class", "").addClass("videoPanelsHorizontal-" + videoPanelsHorizontal).addClass("videoPanelsVertical-" + videoPanelsVertical);
+        }
+
+        function setVideoSizes() {
+            updateVideoPanels();
+        }
+        var $window = $(window),
+            $controls = $("#controls"),
+            $addUrl = $("#addUrl"),
+            $videos = $("#videos"),
+            loaded = [];
+        $controls.scrollToTop();
+        updateVideoPanels();
+        $addUrl.click(function(evt) {
+            var $target = $(evt.target),
+                $urlInput = addUrlInput(),
+                $videoPanel = addVideoPanel($urlInput);
+        });
+        $window.on("change", function(evt) {
+            function loadUrl() {
+                $videoPanel.attr("src", url);
+            }
+            var $target = $(evt.target),
+                random = $target.data("random"),
+                url = $target.val(),
+                $videoPanel = $target.data("$videoPanel");
+            $videos.scrollToTop();
+            loadUrl();
+        });
+    });
+}(window, window.jQuery));
